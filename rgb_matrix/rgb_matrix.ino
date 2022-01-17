621e4a392da64e2beb0cd034785f2d6fbe8d1c24
David Plass <dplassgit@users.noreply.github.com>
2022-01-17 16:46:41 -0500

    Simplified bit twiddling logic.

--- rgb_matrix/rgb_matrix.ino
+++ rgb_matrix/rgb_matrix.ino
@@ -17,7 +17,7 @@ void setup() {
   pinMode(LATCH, OUTPUT); // LATCH pin as output
   pinMode(OE, OUTPUT); // Output Enable
 
-  analogWrite(OE, 0); // Set brightness to full 
+  analogWrite(OE, 0); // Set brightness to full
 }
 
 // lower 4 bits of data1: active low row select, xxxxabcd a=top d=bottom
@@ -25,47 +25,22 @@ void setup() {
 // data0 has color of the column in pairs: bbbbgggg (12341234) active high
 
 // 1. row (active low) in data1 xxxxrrrr
-// 2. color (active high) for that column: red in data1 rrrrxxxx, blue and green in data0 bbbbgggg
+// 2. color (active high) for each column: red in data1 rrrrxxxx, blue and green in data0 bbbbgggg
 void turnOn(int row, int column, int red, int green, int blue) {
   int data1 = 0;
-  switch (row) {
-    // i am so lazy
-    case 0:
-      data1 = 0b00001110;
-      break;
-    case 1:
-      data1 = 0b00001101;
-      break;
-    case 2:
-      data1 = 0b00001011;
-      break;
-    case 3:
-      data1 = 0b00000111;
-      break;
-  }
+  // row is active low, so have to ~ it, but colors are active high so have to & it.
+  data1 = (~(1 << row)) & 0b00001111;
   int data0 = 0;
-  switch (column) {
-    case 0:
-      data1 |= 0b00010000 * red;
-      data0 |= 0b00010000 * blue;
-      data0 |= 0b00000001 * green;
-      break;
-    case 1:
-      data1 |= 0b00100000 * red;
-      data0 |= 0b00100000 * blue;
-      data0 |= 0b00000010 * green;
-      break;
-    case 2:
-      data1 |= 0b01000000 * red;
-      data0 |= 0b01000000 * blue;
-      data0 |= 0b00000100 * green;
-      break;
-    case 3:
-      data1 |= 0b10000000 * red;
-      data0 |= 0b10000000 * blue;
-      data0 |= 0b00001000 * green;
-      break;
+  if (red) {
+    data1 |= 0b00010000 << column;
   }
+  if (green) {
+    data0 |= 1 << column;
+  }
+  if (blue) {
+    data0 |= 0b00010000 << column;
+  }
+
   digitalWrite(LATCH, LOW);
   shiftOut(SER, CLK, LSBFIRST, data0);
   shiftOut(SER, CLK, LSBFIRST, data1);
@@ -77,8 +52,8 @@ int column = 0;
 int color = 0b001; // bgr
 
 void loop() {
-  // oneLedOnLoop();
-  allLedsOnLoop();
+  oneLedOnLoop();
+//  allLedsOnLoop();
 }
 
 int iteration = 0;
@@ -120,8 +95,8 @@ void oneLedOnLoop() {
     if (color == 8) {
       color = 1;
     }
-    //    Serial.print("Color is now: ");
-    //    Serial.println(color, BIN);
+    Serial.print("Color is now: ");
+    Serial.println(color, BIN);
   }
   delay(200);
 }
